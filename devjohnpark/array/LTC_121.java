2aa2bd8d050487128897dd46926f533f18ba6883
devjohnpark <devjohnpark@gmail.com>
2025-07-31 10:53:23 +0900

    [Comment] add comment for 121

--- devjohnpark/array/LTC_121.java
+++ devjohnpark/array/LTC_121.java
@@ -1,12 +1,16 @@
 class LTC_121 {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int min = prices[0];
+        int maxProfit = 0; // 1. 이익의 최대값을 `0`으로 초기화한다.
+        int min = prices[0]; // 2. 지정된 배열 요소 전까지의 최소값을 첫번째 배열 요소값으로 초기화
         int n = prices.length;
-        for (int i = 1; i < n; i++) {
-            maxProfit = Math.max(prices[i] - min, maxProfit); // 1. 배열을 순회하면서 이익 최대값 업데이트
-            min = Math.min(prices[i], min); // 2. 배열을 순회하면서 요소값중 최솟값 업데이트
+		// 3. 배열을 순회하면서 하나씩 요소값을 지정한다.
+		for (int i = 1; i < n; i++) {
+	        // 4. 지정된 배열 요소값과 지정된 배열 요소 전까지의 최솟값의 차이를 이익의 최대값으로 업데이트
+            maxProfit = Math.max(prices[i] - min, maxProfit); 
+
+			// 5. 지정된 배열 요소값과 지정된 배열 요소 전까지의 최솟값을 비교해서 작은값으로 지정된 배열 요소 전까지의 최솟값을 업데이트
+            min = Math.min(prices[i], min); 
         }
-        return maxProfit; // 3. 이익 최댓값 반환
+        return maxProfit; // 6. 이익의 최댓값 반환
     }
 }
